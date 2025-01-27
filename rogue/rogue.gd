bb3bc31d710c6fa0fcf82ad9b2ee2625665c77f6
Federico Bellia <f.bellia@hotmail.it>
2025-01-28 00:50:12 +0100

    Setup movement handling for character

--- rogue/rogue.gd
+++ rogue/rogue.gd
@@ -1,9 +1,23 @@
 extends Area2D
 
+@export var speed : int = 350
+@onready var _sprite = $RogueSprite
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_sprite.play("idle")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var move = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_up"):
+		move += Vector2.UP
+	if Input.is_action_pressed("move_down"):
+		move += Vector2.DOWN
+	if Input.is_action_pressed("move_right"):
+		move += Vector2.RIGHT
+	if Input.is_action_pressed("move_left"):
+		move += Vector2.LEFT
+	
+	position += move.normalized() * speed * delta
