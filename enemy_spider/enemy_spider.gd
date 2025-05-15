6e0d9eec719775f2aebc231dbb1027ef74ee4689
Federico Bellia <f.bellia@hotmail.it>
2025-05-15 23:15:25 +0200

    Use player group instead of direct reference

--- enemy_spider/enemy_spider.gd
+++ enemy_spider/enemy_spider.gd
@@ -2,14 +2,14 @@ class_name EnemySpider
 extends Area2D
 
 @export var speed = 1.0
-@export_storage var player : Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var player := get_tree().get_first_node_in_group("player")
 	var target : Vector2 = player.position
 	var distance = target - position
 	var direction = distance.normalized()
--- main.gd
+++ main.gd
@@ -4,7 +4,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$EnemySpider.player = _char
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
